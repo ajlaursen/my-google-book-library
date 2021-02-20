015e9787b99063fec21d190ed3a0b6db84f2a2a4
Alex Laursen <ajlaursen@aol.com>
2021-02-20 14:05:25 -0500

    added onRemove

--- Client/src/utils/CART.js
+++ Client/src/utils/CART.js
@@ -1,17 +1,28 @@
 
 
-    function onAdd (product, cartItems, setCartItems)  {
-        const exists = cartItems.find(x => x.id === product.id);
-        if (exists){
-            setCartItems(cartItems.map( (x) => 
-                x.id === product.id ? {...exists, qty: exists.qty +1} : x
-                )
-            );
-        } else {
-            setCartItems([...cartItems, { ...product, qty: 1 }])
-        }
+function onAdd (product, cartItems, setCartItems)  {
+    const exists = cartItems.find(x => x.id === product.id);
+    if (exists){
+        setCartItems(cartItems.map( (x) => 
+            x.id === product.id ? {...exists, qty: exists.qty +1} : x
+            )
+        );
+    } else {
+        setCartItems([...cartItems, { ...product, qty: 1 }])
     }
+}
 
+function onRemove(product, cartItems, setCartItems) {
+    const exists = cartItems.find((x) => x.id === product.id)
+    if (exists.qty === 1){
+        setCartItems(cartItems.fitler((x) => x.id !==product.id));
+    }else{
+        setCartItems(cartItems.map( (x) => 
+            x.id === product.id ? {...exists, qty: exists.qty -1} : x
+            )
+        );
+    }
+}
+    
 
-export const cart = { onAdd }
-
+export const cart = { onAdd, onRemove }
